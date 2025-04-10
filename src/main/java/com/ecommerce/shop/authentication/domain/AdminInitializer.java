15ba46e47a195df4b123885dbfc34639901a7c25
pbrzozow <180855918+pbrzozow@users.noreply.github.com>
2025-04-10 11:04:20 +0200

    fix admin password encoding

--- src/main/java/com/ecommerce/shop/authentication/domain/AdminInitializer.java
+++ src/main/java/com/ecommerce/shop/authentication/domain/AdminInitializer.java
@@ -2,20 +2,24 @@ package com.ecommerce.shop.authentication.domain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class AdminInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) {
         if (userRepository.findUserByRole(Role.ADMIN).isEmpty()) {
             User admin = User.builder()
                     .email("a@a")
-                    .password("aaa")
+                    .password(passwordEncoder.encode("aaa"))
                     .role(Role.ADMIN)
                     .build();
             userRepository.save(admin);
